1e2c756affd89b7b62b5ea8d133dd924fd9b3be0
Nimish Kumar <nimishkumar.india111@gmail.com>
2026-03-17 23:39:40 +0530

    updated text editor

--- client/src/features/blog/components/tiptap-templates/simple/simple-editor.jsx
+++ client/src/features/blog/components/tiptap-templates/simple/simple-editor.jsx
@@ -1,12 +1,12 @@
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Link from '@tiptap/extension-link';
-import Table from '@tiptap/extension-table';
-import TableCell from '@tiptap/extension-table-cell';
-import TableHeader from '@tiptap/extension-table-header';
-import TableRow from '@tiptap/extension-table-row';
+import { Table } from '@tiptap/extension-table';
+import { TableCell } from '@tiptap/extension-table-cell';
+import { TableHeader } from '@tiptap/extension-table-header';
+import { TableRow } from '@tiptap/extension-table-row';
 import { Color } from '@tiptap/extension-color';
-import TextStyle from '@tiptap/extension-text-style';
+import { TextStyle } from '@tiptap/extension-text-style';
 import { useCallback, useEffect, useState } from 'react';
 import './simple-editor.css';
 const MenuBar = ({ editor }) => {
